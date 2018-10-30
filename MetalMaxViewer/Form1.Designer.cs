6dff8d90c0911ea74ec695c6cb3590ebe3aa9d4f
joguersan <joguersan@gmail.com>
2018-10-30 20:40:19 +0100

    Versión inicial. El programa usa un archivo de prueba, no se puede elegir archivo. Preview de texto jap y eng. Cambio de fragmentos dentro de una intervención.

---
+++ MetalMaxViewer/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace MetalMaxViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox2.Location = new System.Drawing.Point(35, 338);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(384, 288);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::MetalMaxViewer.Properties.Resources.bocadillo;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(35, 26);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(384, 288);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(1039, 26);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 596);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(505, 110);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(481, 173);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(505, 362);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(481, 173);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(683, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(749, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(587, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "<--";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(820, 303);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "-->";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(683, 557);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(749, 557);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1181, 654);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        public System.Windows.Forms.PictureBox pictureBox2;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MetalMaxViewer/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Yarhl.IO;
+using Yarhl.Media.Text;
+using Yarhl.FileFormat;
+using System.Drawing.Text;
+
+namespace MetalMaxViewer
+{
+
+
+    public partial class Form1 : Form
+    {
+        // Translated text
+        public Form1()
+        {
+            InitializeComponent();
+            string archivo = @"C:\Users\trans\Desktop\PO\xls\npc\C01.MSG.po";
+            ImportPO(archivo);
+            cambioImagen(archivo);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {   
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label1.Text = "1";
+            label3.Text = "1";
+            printText();
+        }
+        private void printText()
+        {
+            richTextBox1.Text = variables.textoOriginal[listBox1.SelectedIndex];
+            cambioPosicion(0, listBox1.SelectedIndex);
+            richTextBox2.Text = variables.textoTraducido[listBox1.SelectedIndex];
+            label2.Text = variables.splittedOr.Length.ToString();
+            label4.Text = variables.splittedMod.Length.ToString();
+        }
+        public void ImportPO(string poFileName)
+        {
+            DataStream inputPO = new DataStream(poFileName, FileOpenMode.Read);
+            BinaryFormat binaryFile = new BinaryFormat(inputPO);
+            Po newPO = binaryFile.ConvertTo<Po>();
+            inputPO.Dispose();
+            int contador = 0;
+            foreach (var entry in newPO.Entries)
+            {
+                listBox1.Items.Add(entry.Context);
+                variables.textoOriginal[contador] = entry.Original;
+
+                if (entry.Translated == "")
+                {
+                    variables.textoTraducido[contador] = "<!empty>";
+                }
+                variables.textoTraducido[contador] = entry.Translated;
+                contador++;
+            }
+        }
+        public static class variables
+        {
+            public static string[] textoOriginal = new string[5000];
+            public static string[] textoTraducido = new string[5000];
+            public static string[] splittedOr = new string[150];
+            public static string[] splittedMod = new string[150];
+            public static string[] contexto = new string[5000];
+        }
+        public void cambioImagen(string contenido)
+        {
+            if (contenido.Contains(".MSG."))
+            {
+                pictureBox1.BackgroundImage = MetalMaxViewer.Properties.Resources.bocadillo2;
+                pictureBox1.BackgroundImageLayout=ImageLayout.Stretch;
+                pictureBox2.BackgroundImage = MetalMaxViewer.Properties.Resources.bocadillo2;
+                pictureBox2.BackgroundImageLayout = ImageLayout.Stretch;
+            }
+        }
+        public void cambioPosicion(int pos, int posText)
+        {
+            if (pos==0)
+            {
+                colocarText(variables.textoOriginal[posText], variables.textoTraducido[posText], 10, 175);
+            }
+        }
+        public void colocarText(string texto1, string texto2, int x, int y)
+        {
+            int posJap = Int32.Parse(label1.Text);
+            int posEn = Int32.Parse(label3.Text);
+            posJap--;
+            posEn--;
+            splitText(texto1, texto2);
+            File.WriteAllBytes("verdana2.ttf", Properties.Resources.PixelFJVerdana12pt);
+            var font = new Font(@"verdana2.ttf", 15.5f, FontStyle.Regular, GraphicsUnit.Pixel);
+            var image = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+            var graphics = Graphics.FromImage(image);
+            graphics.DrawString(variables.splittedOr[posJap], font, Brushes.White, new Point(x, y));
+            this.pictureBox1.Image = image;
+            var image2 = new Bitmap(this.pictureBox2.Width, this.pictureBox2.Height);
+            var graphics2 = Graphics.FromImage(image2);
+            graphics2.DrawString(variables.splittedMod[posEn], font, Brushes.White, new Point(x, y));
+            this.pictureBox2.Image = image2;
+        }
+
+        public void splitText(string texto1, string texto2)
+        {
+            Array.Clear(variables.splittedOr, 0, variables.splittedOr.Length);  
+            string separador = "{END TEXTBOX}";
+            variables.splittedOr = texto1.Split(new string[] { separador }, StringSplitOptions.None);
+            variables.splittedMod = texto2.Split(new string[] { separador }, StringSplitOptions.None);
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            cambioPosicion(0, listBox1.SelectedIndex);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int et1 = Int32.Parse(label1.Text);
+            int et3 = Int32.Parse(label3.Text);
+            if (et1 > 1 && !(et1 < et3))
+                et1--;
+            if (et3 > 1 && !(et1 > et3))
+                et3--;
+            label1.Text = et1.ToString();
+            label3.Text = et3.ToString();
+            printText();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int et1 = Int32.Parse(label1.Text);
+            int et3 = Int32.Parse(label3.Text);
+            if (et1 < variables.splittedOr.Length)
+                et1++;
+            if (et3 < variables.splittedMod.Length)
+                et3++;
+            label1.Text = et1.ToString();
+            label3.Text = et3.ToString();
+            printText();
+        }
+    }
+}
